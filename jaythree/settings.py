5d7aa1f5a216bf97f665ca576afde37a6ae6d686
gregatemi <greg.atemi@systechafrica.com>
2024-11-23 09:48:02 +0300

    Added DevOps tooling

--- jaythree/settings.py
+++ jaythree/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'jaythree.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "jaythree",
-#         'USER': "greg",
-#         'PASSWORD': "azerty254",
-#         'HOST': "localhost",
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "jaythree",
+        'USER': "greg",
+        'PASSWORD': "azerty254",
+        'HOST': "localhost",
+        'PORT': 5432,
+    }
+}
 
 # DATABASES = {
 #     'default': {
